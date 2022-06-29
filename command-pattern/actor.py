6d0b3acedf2ee08786754c9f1382303210bdffce
Jarret Nachtigal <jarretn1224@gmail.com>
2022-06-29 14:48:21 -0400

    actor and child classes should be working

--- command-pattern/actor.py
+++ command-pattern/actor.py
@@ -3,15 +3,27 @@ class Actor:
     position = None
     jump_count = None
 
+    # override
+    def jump(self):
+        self.jump_count += 1
+        # raise NotImplementedError()  # makes sure its overridden
+
+    # override
+    def move(self):
+        self.position += 1
+        # raise NotImplementedError()  # makes sure its overridden
+
 
 # not gonna make another file for these
 class PlayableCharacter(Actor):
     def __init__(self) -> None:
         self.position = 0
         self.jump_count = 0
+    # uses parent class jump and move
 
 
 class AICharacter(Actor):
     def __init__(self) -> None:
         self.position = 10
         self.jump_count = 0
+    # uses parent class jump and movE
