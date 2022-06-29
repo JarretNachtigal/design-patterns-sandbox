bdedd92c5f431acb95bbce14e1e84bfcdf49fcc8
Jarret Nachtigal <jarretn1224@gmail.com>
2022-06-29 14:43:59 -0400

    jump and move functions called, not written yet

--- command-pattern/command.py
+++ command-pattern/command.py
@@ -10,40 +10,46 @@ import actor
 # So by saying the Command pattern is a “reified method call”, what I mean is that
 # it’s a method call wrapped in an object."
 
+
 # "represents a triggerable game command"
 # parent class for all game commands
-
-
+# essentially, this class can be used for movement/ commands for all actors, not just a playable character
+# by taking in the actor as a parameter
 class Command():
     def __init__(self) -> None:
         pass
 
     # will be overridden
     def command():
-        raise NotImplementedError()
+        raise NotImplementedError()  # makes sure its overridden
 
     # will be overridden
-    def execute():
-        pass
+    def execute(actor):
+        raise NotImplementedError()  # makes sure its overridden
 
 
 # will make a character jump. for simplicities sake, it will increase an integer
 # called jump_counter by 1
 class JumpCommand(Command):
-    def __init__(self) -> None:
-        pass
-        # will be overridden
 
+    # what is this for?
     def command():
         pass
 
+    # calls the actor specific method
+    def execute(actor):
+        actor.jump()
 
 # will move a character. for simplicities sake, it will increase an integer
 # called position by 1
-class MoveCommand(Command):
-    def __init__(self) -> None:
-        pass
 
-    # will be overridden
+
+class MoveCommand(Command):
+
+    # what is this for?
     def command():
         pass
+
+    # calls the actor specific method
+    def execute(actor):
+        actor.move()
