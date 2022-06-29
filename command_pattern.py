d8db31d4b6d7cea7eacce17fdb2990b0d492fbdf
Jarret Nachtigal <jarretn1224@gmail.com>
2022-06-29 13:51:34 -0400

    setup and initial commit for command_patters.py

---
+++ command_pattern.py
@@ -0,0 +1,11 @@
+# this file contains my attempt/notes at implementing the command design pattern in
+# chapter 2 of https://gameprogrammingpatterns.com/contents.html
+# --------------------------------------------------------------------------------
+# NOTES
+#
+# "A command is a reified method call."
+#
+# "Both terms mean taking some concept and turning it into a piece of data
+#  — an object — that you can stick in a variable, pass to a function, etc.
+# So by saying the Command pattern is a “reified method call”, what I mean is that
+# it’s a method call wrapped in an object."
