09211518230b13e40074c4dc6fc8c767a0af87a3
Jarret Nachtigal <jarretn1224@gmail.com>
2022-06-30 13:44:27 -0400

    adds move backwards to input handler

--- command-pattern/input_handler.py
+++ command-pattern/input_handler.py
@@ -7,12 +7,16 @@ class InputHandler():
     def __init__(self) -> None:
         self.MOVE_COMMAND = 'w'
         self.JUMP_COMMAND = ' '
+        self.MOVE_BACK_COMMAND = "s"
 
     # this method delegates
     def handleInput(self, s, actor):
         if s == self.MOVE_COMMAND:
             # send move command
             command.MoveCommand.execute(actor)
+        elif s == self.JUMP_COMMAND or 's':
+            # send move backwards command
+            command.MoveBackwardsCommand.execute(actor)
         elif s == self.JUMP_COMMAND or 'space':
             # send jump command
             command.JumpCommand.execute(actor)
