e5941f929b6c65558f9fba5f5c28455eb1255a35
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-01 13:41:51 -0400

    adds main

--- flyweight-pattern/main.py
+++ flyweight-pattern/main.py
@@ -1,3 +1,10 @@
 # this folder contains my attempt/notes at implementing the flyweight design pattern in
 # chapter 2 of https://gameprogrammingpatterns.com/contents.html
 # comments will be braindead obvious, but are for note taking purposes
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
