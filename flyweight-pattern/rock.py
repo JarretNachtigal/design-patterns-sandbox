aabf45ca1eccea344abdc7bfff75dc00755a77ff
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-01 14:10:08 -0400

    setup Rock class

---
+++ flyweight-pattern/rock.py
@@ -0,0 +1,13 @@
+class Rock:
+    def __init__(self, shared_resource, position, size_modifier) -> None:
+        # color and texture from SharedResouce instance, should all reference the same one
+        self.shared_resource = shared_resource
+        # instance variables for each rock
+        self.position = position
+        self.size_modifier = size_modifier
+
+    def get_position(self):
+        return self.position
+
+    def get_size_modifier(self):
+        return self.size_modifier
