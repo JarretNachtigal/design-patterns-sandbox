36e725893284e8cb0a6cd9b9786849496f4054dc
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-08 14:37:08 -0400

    comments

--- prototype-pattern/enemy_generator.py
+++ prototype-pattern/enemy_generator.py
@@ -1,12 +1,10 @@
 import enemy
 
 
+# can hold a prototype of any enemy and then spawn new ones with the same attributes
 class EnemyGenerator:
     def __init__(self, prototype: enemy.Enemy) -> None:
         self.prototype = prototype
 
-    # i guess this is how this works. probably could be built to be more reusable with
-    # better methods for different enemy types, ex: a method in each enemy type that returns all its data
-    # instead of individual generators
     def spawn(self):
         return self.prototype.clone()
