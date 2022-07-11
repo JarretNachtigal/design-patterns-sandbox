c822af42baed3b82b35fb68c2e81e2b3aa685011
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-11 15:05:22 -0400

    comments

--- state-pattern/main.py
+++ state-pattern/main.py
@@ -1,5 +1,5 @@
-# The goal of the State pattern is to encapsulate
-#  all of the behavior and data for one state in a single class.
+# "The goal of the State pattern is to encapsulate
+# all of the behavior and data for one state in a single class."
 def main():
     pass
 
