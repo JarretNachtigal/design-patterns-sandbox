5a26976cf5709ee5c79b93adae0becaa2b531152
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-11 15:04:24 -0400

    adds files for state pattern

---
+++ state-pattern/main.py
@@ -0,0 +1,8 @@
+# The goal of the State pattern is to encapsulate
+#  all of the behavior and data for one state in a single class.
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
