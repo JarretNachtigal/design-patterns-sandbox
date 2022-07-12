8597a2f662d6d2264d3e384b8b7a19c51a2087c8
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-12 15:14:09 -0400

    began character class

---
+++ state-pattern/character.py
@@ -0,0 +1,11 @@
+import character_state
+
+
+# class used to create the player character object
+# delegates handle input and update methods to character_state
+# and its subclasses, which will be stored in an instance variable
+class Character:
+    def __init__(self, hp) -> None:
+        self.hp = hp
+        self.state = character_state.StandingState()  # character spawns standing
+        pass
