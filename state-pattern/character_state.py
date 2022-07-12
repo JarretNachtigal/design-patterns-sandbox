5e9f7b7398f5852f9dad64e1eeb54da3357da2fa
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-12 15:08:47 -0400

    class structure

---
+++ state-pattern/character_state.py
@@ -0,0 +1,24 @@
+class CharacterState:
+
+    def __init__(self) -> None:
+        pass
+
+    #
+    def handle_input():
+        pass
+
+    # would be called once per game loop
+    def update():
+        pass
+
+
+class JumpingState(CharacterState):
+    pass
+
+
+class DuckingState(CharacterState):
+    pass
+
+
+class StandingState(CharacterState):
+    pass
