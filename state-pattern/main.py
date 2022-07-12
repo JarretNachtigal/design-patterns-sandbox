f04825b1371cf067fa81acb232ba8e95c848f375
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-12 15:57:14 -0400

    comments

--- state-pattern/main.py
+++ state-pattern/main.py
@@ -1,9 +1,12 @@
 import character
-# import character_state
 
 
 # "The goal of the State pattern is to encapsulate
 # all of the behavior and data for one state in a single class."
+# this implementation uses static states
+# there is another way of implementing the CharacterStates (instantiated states)
+# in which a new object is created at runtime. this is needed when the variables inside that
+# object are reset on each press ex: charge attack timer
 def main():
     # create character
     hp = 2
