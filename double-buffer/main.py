11c0798c1e7f1f92b6006673e62f36c71fc25e1f
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-13 13:44:17 -0400

    more comments

--- double-buffer/main.py
+++ double-buffer/main.py
@@ -1,4 +1,5 @@
 # this file contains the main method for the double buffer pattern
+# https://gameprogrammingpatterns.com/double-buffer.html
 # "A buffered class encapsulates a buffer: a piece of state that can be modified.
 # This buffer is edited incrementally, but we want all outside code to see the edit
 # as a single atomic change. To do this, the class keeps two instances of the buffer:
