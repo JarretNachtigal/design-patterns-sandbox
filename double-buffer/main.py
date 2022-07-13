3d5e49f86ccbcb8eec1a836646ecad66cf0a07c9
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-13 13:43:46 -0400

    more comments

--- double-buffer/main.py
+++ double-buffer/main.py
@@ -1,4 +1,8 @@
 # this file contains the main method for the double buffer pattern
+# "A buffered class encapsulates a buffer: a piece of state that can be modified.
+# This buffer is edited incrementally, but we want all outside code to see the edit
+# as a single atomic change. To do this, the class keeps two instances of the buffer:
+# a next buffer and a current buffer."
 def main():
     pass
 
