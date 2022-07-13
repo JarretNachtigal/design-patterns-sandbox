3ce8d5bddfae9df3a3eedc50b156a5dc6d5ec342
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-13 13:46:47 -0400

    more comments

--- double-buffer/main.py
+++ double-buffer/main.py
@@ -12,8 +12,11 @@ if __name__ == "__main__":
 # This buffer is edited incrementally, but we want all outside code to see the edit
 # as a single atomic change. To do this, the class keeps two instances of the buffer:
 # a next buffer and a current buffer."
+
 # "More specifically, this pattern is appropriate when all of these are true:
 # - We have some state that is being modified incrementally.
 # - That same state may be accessed in the middle of modification.
 # - We want to prevent the code that’s accessing the state from seeing the work in progress.
-# - We want to be able to read the state and we don’t want to have to wait while it’s being written.""
+# - We want to be able to read the state and we don’t want to have to wait while it’s being written."
+
+# "no code can access either state while they are being swapped."
