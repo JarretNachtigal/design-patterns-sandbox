704d324130ccebd12b9750e5e61aaa2d56b9f996
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-14 15:05:25 -0400

    Screen init and draw()

--- double-buffer/screen.py
+++ double-buffer/screen.py
@@ -1,3 +1,11 @@
+import frame_buffer
 # holds both FrameBuffers and draw method
+
+
 class Screen:
-    pass
+    def __init__(self):
+        current_buffer = frame_buffer.FrameBuffer()
+        next_buffer = frame_buffer.FrameBuffer()
+
+    def draw():
+        pass
