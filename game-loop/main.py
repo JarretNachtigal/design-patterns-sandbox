44029b84931d3f2855a813e55e071dada6bbc3a1
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-15 14:06:49 -0400

    comments planning main again

--- game-loop/main.py
+++ game-loop/main.py
@@ -1,6 +1,8 @@
 # this file will hold the main method for the game loop pattern
 # essentially it is a while loop that calls the update method(s)
 # for each tic of in game time
+# in prebuild engines like unity, this is already implemented and needs to
+# be worked with correctly
 
 
 def main():
@@ -8,8 +10,8 @@ def main():
     # - update and render should not wait for process input / game time should not wait for a user to input
     # while loop:
     # process_input()
-    # update()
-    # render()
+    # update() # state pattern?
+    # render() # double buffer?
     pass
 
 
