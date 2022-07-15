7f2b27443d1b5f0d31f71c17d6818e3b79492b2d
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-15 14:20:59 -0400

    comments and main_loop_two added to show fixed time step

--- game-loop/main.py
+++ game-loop/main.py
@@ -13,22 +13,50 @@ def main():
     current_time = 0
     last_time = 0
 
-    # while loop:
+    # while(true) loop:
 
-    # get current time
     current_time += 1  # this should call a method to get current time
     # get time between last and current loop iteration
     time_elapsed = current_time - last_time
 
-    # process_input()
-
+    process_input()
     update(time_elapsed)  # state pattern?
     render()  # double buffer?
 
     # sleep for needed amount of time to keep frame rate where
     # it needs it - depends on how long the loop takes to run compared to time at beginning
     last_time = current_time
-    pass
+
+
+# this version has a fixed time step - decouple render and update timing
+# the rendering and input processing happen every tic, the game state update does not
+def main_loop_two():
+    # this needs to run (asynchoronously???)
+    # - update and render should not wait for process input / game time should not wait for a user to input
+
+    # save current and last time of loop iteration
+    current_time = 0
+    lag = 0.0  # used for fixed time step
+    last_time = 0
+
+    # while(true) loop:
+
+    current_time += 1  # this should call a method to get current time
+    # get time between last and current loop iteration
+    time_elapsed = current_time - last_time
+    last_time = current_time
+    lag += time_elapsed  # sync ingame and real world time
+
+    process_input()
+
+    # while(lag >= MS_PER_UPDATE)
+    update(time_elapsed)  # state pattern?
+    # end loop
+
+    render()  # double buffer?
+
+    # sleep for needed amount of time to keep frame rate where
+    # it needs it - depends on how long the loop takes to run compared to time at beginning
 
 
 # update the game state based on time elapsed since last call
