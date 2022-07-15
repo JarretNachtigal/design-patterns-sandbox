10c2964261dc918a398507120bb4ae2d366eb72d
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-15 14:04:44 -0400

    comments planning main

--- game-loop/main.py
+++ game-loop/main.py
@@ -2,7 +2,14 @@
 # essentially it is a while loop that calls the update method(s)
 # for each tic of in game time
 
+
 def main():
+    # this needs to run (asynchoronously???)
+    # - update and render should not wait for process input / game time should not wait for a user to input
+    # while loop:
+    # process_input()
+    # update()
+    # render()
     pass
 
 
