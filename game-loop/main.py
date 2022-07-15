2ba2e16564ddf3b9bd741d39742bfce441a9ca53
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-15 14:02:39 -0400

    adds main  method and comments

---
+++ game-loop/main.py
@@ -0,0 +1,10 @@
+# this file will hold the main method for the game loop pattern
+# essentially it is a while loop that calls the update method(s)
+# for each tic of in game time
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
