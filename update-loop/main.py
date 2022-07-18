7cacf745847fabc54ebf640b155596d4a31164ef
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-18 15:21:46 -0400

    comments

--- update-loop/main.py
+++ update-loop/main.py
@@ -1,1 +1,5 @@
 # "each entity in the game should encapsulate its own behavior.""
+# "Once per frame, the game loop walks the collection and calls update()
+#  on each object. This gives each one a chance to perform one frame’s worth
+# of behavior. By calling it on all objects every frame,
+# they all behave simultaneously."
