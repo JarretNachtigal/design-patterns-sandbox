80dab8488aef58b5aef954fad039c21649e288cd
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-18 15:24:11 -0400

    comments

--- update-loop/main.py
+++ update-loop/main.py
@@ -1,5 +1,12 @@
-# "each entity in the game should encapsulate its own behavior.""
+# "each entity in the game should encapsulate its own behavior."
+
 # "Once per frame, the game loop walks the collection and calls update()
 #  on each object. This gives each one a chance to perform one frame’s worth
 # of behavior. By calling it on all objects every frame,
 # they all behave simultaneously."
+
+# "Update methods work well when:
+
+# - Your game has a number of objects or systems that need to run simultaneously.
+# - Each object’s behavior is mostly independent of the others.
+# - The objects need to be simulated over time."
