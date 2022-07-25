367aa834336f1baa7c0edd27eae94a35d77cc58e
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-25 14:13:23 -0400

    adds push() to stack

--- bytecode-pattern/stack.py
+++ bytecode-pattern/stack.py
@@ -4,3 +4,7 @@ from mimetypes import init
 class Stack:
     def __init__(self) -> None:
         self.data = []
+    # add 1 value to top of stack
+
+    def push(self, value):
+        self.data.append(value)
