d7de6886108a291ce607ed80373e41c90baf360d
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-25 14:25:00 -0400

    adds Character stat set() methods

--- bytecode-pattern/character.py
+++ bytecode-pattern/character.py
@@ -1,7 +1,27 @@
 # basic character class with 3 fields
 # atk, hp, defence
+# to interface with the stack instructions, it will heve general methods to manipulating
+# its stats/state
+from re import L
+
+
 class Character:
     def __init__(self, hp, atk, defence) -> None:
         self.hp = hp
         self.atk = atk
         self.defence = defence
+
+    # these methods will take a character to manipulate as an argument rather
+    # than useing self
+
+    @staticmethod
+    def setHealth(character, change_val):
+        return character
+
+    @staticmethod
+    def setHp(character, change_val):
+        return character
+
+    @staticmethod
+    def setDefence(character, change_val):
+        return character
