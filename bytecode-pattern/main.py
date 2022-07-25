7b903f405240df0624fad0f99d0c33d6752b4680
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-25 14:17:55 -0400

    comments

--- bytecode-pattern/main.py
+++ bytecode-pattern/main.py
@@ -1,7 +1,16 @@
 import stack
 
+# this pattern involves creating building blocks that can be controlled
+# with bytecode
+# the instructions[] list will hold letters and numbers, representing valid
+# instructions for the emulator
+# it will be stepped through one char at a time for simplicity and mimic a user creating
+# behavior by manipulating the stack at runtime
+# 2 characters will be used to demonstrate
+
 
 def main():
+    instructions = []
     pass
 
 
