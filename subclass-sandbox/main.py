ea48a9ddfe5bcca9a5795f4d49b74e602dbbb0b5
Jarret Nachtigal <jarretn1224@gmail.com>
2022-07-26 15:54:52 -0400

    comments

--- subclass-sandbox/main.py
+++ subclass-sandbox/main.py
@@ -1,2 +1,5 @@
-# Let’s say we unleash our team and get them writing superpower classes. What’s going to happen?
-# - `There will be lots of redundant code.
+# "Let’s say we unleash our team and get them writing superpower classes. What’s going to happen?
+# - There will be lots of redundant code.
+# - Every part of the game engine will get coupled to these classes.
+# - When these outside systems need to change, odds are good some random superpower code will get broken.
+# - It’s hard to define invariants that all superpowers obey."
