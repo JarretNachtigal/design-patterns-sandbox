60d3660bfbe30f9cacd7cf7326f9d3c710c48f4f
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-01 13:43:49 -0400

    comments/notes

--- component-pattern/main.py
+++ component-pattern/main.py
@@ -1,2 +1,7 @@
 # "Components are basically plug-and-play for objects. They let us build complex entities with rich behavior
 # by plugging different reusable component objects into sockets on the entity."
+
+# "let’s try it with components. Our subclasses disappear completely.
+# Instead, we have a single GameObject class and two component classes: PhysicsComponent and GraphicsComponent.
+# A decoration is simply a GameObject with a GraphicsComponent but no PhysicsComponent.
+# A zone is the opposite, and a prop has both components."
