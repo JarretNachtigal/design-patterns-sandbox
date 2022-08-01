8215282b8628f5b88f934cdff3dc8303d7ac9b17
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-01 13:44:10 -0400

    comments/notes

--- component-pattern/main.py
+++ component-pattern/main.py
@@ -5,3 +5,5 @@
 # Instead, we have a single GameObject class and two component classes: PhysicsComponent and GraphicsComponent.
 # A decoration is simply a GameObject with a GraphicsComponent but no PhysicsComponent.
 # A zone is the opposite, and a prop has both components."
+
+# "A single entity spans multiple domains."
