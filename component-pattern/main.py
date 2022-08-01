286f662df38c1e130b71b836f2b92cb1574c94a9
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-01 13:43:13 -0400

    comments/notes

---
+++ component-pattern/main.py
@@ -0,0 +1,2 @@
+# "Components are basically plug-and-play for objects. They let us build complex entities with rich behavior
+# by plugging different reusable component objects into sockets on the entity."
