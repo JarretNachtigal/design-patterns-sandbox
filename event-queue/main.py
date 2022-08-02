8936e7396729b65914640f99e03a4b9c361a6dff
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-02 14:28:35 -0400

    add event queue notes

--- event-queue/main.py
+++ event-queue/main.py
@@ -2,3 +2,9 @@
 # reflects how the world was when the event was raised. This means queued events tend to be more data heavy
 # than events in synchronous systems. With the latter, the notification can say “something happened” and
 # the receiver can look around for the details. "
+
+# "All event and message systems have to worry about cycles:
+# - A sends an event.
+# - B receives it and responds by sending an event.
+# - That event happens to be one that A cares about, so it receives it. In response, it sends an event…
+# - Go to 2."
