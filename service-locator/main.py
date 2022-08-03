7f2f65f722f100c8b2c5a5eccafb02b06c9d8c15
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-03 13:24:11 -0400

    comments/notes

--- service-locator/main.py
+++ service-locator/main.py
@@ -1,1 +1,5 @@
 # "Provide a global point of access to a service without coupling users to the concrete class that implements it."
+
+# static class and singleton work but also introduce coupling
+
+# "phone book"
