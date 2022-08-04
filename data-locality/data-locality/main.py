859a473acde20dfa73efbe2170e3952b236d9c5e
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-04 13:07:35 -0400

    notes

--- data-locality/data-locality/main.py
+++ data-locality/data-locality/main.py
@@ -1,1 +1,5 @@
 # "organize your data structures so that the things you’re processing are next to each other in memory."
+
+# "Modern computers have a little chunk of memory right inside the chip. The CPU can pull data from this much faster than it can from main memory."
+
+""
