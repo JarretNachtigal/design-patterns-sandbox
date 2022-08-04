3c02d83b95d9ea795f449618693d53c1ca4cfa73
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-04 13:07:45 -0400

    notes

--- data-locality/data-locality/main.py
+++ data-locality/data-locality/main.py
@@ -2,4 +2,5 @@
 
 # "Modern computers have a little chunk of memory right inside the chip. The CPU can pull data from this much faster than it can from main memory."
 
-""
+# "Whenever your chip needs a byte of data from RAM, it automatically grabs a whole chunk of contiguous memory — usually around 64 to 128 bytes — and puts it in the cache.
+# This dollop of memory is called a cache line."
