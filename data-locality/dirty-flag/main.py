3ee28bd02f77dc02a3a6071bf467362ba02622c6
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-08 14:19:50 -0400

    notes

--- data-locality/dirty-flag/main.py
+++ data-locality/dirty-flag/main.py
@@ -3,3 +3,12 @@
 # local transform - movement of object - moving a hand
 
 # world transform - movement of object and all related objects in hierarchy - moving a hand in a moving car
+
+# "We need the world transform for every object in the world every frame, so even though there are only a handful of matrix
+# multiplications per model, it’s on the hot code path where performance is critical."
+
+# SLOW!
+# "The simplest approach is to calculate transforms on the fly while rendering.
+# Each frame, we recursively traverse the scene graph starting at the top of the hierarchy. "
+
+# "The obvious answer is to cache it. "
