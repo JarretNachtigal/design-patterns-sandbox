6818816ad3729d3586239a856c4bad45411493d4
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-08 14:18:04 -0400

    notes

--- data-locality/dirty-flag/main.py
+++ data-locality/dirty-flag/main.py
@@ -1,1 +1,5 @@
 # "Avoid unnecessary work by deferring it until the result is needed."
+
+# local transform - movement of object - moving a hand
+
+# world transform - movement of object and all related objects in hierarchy - moving a hand in a moving car
