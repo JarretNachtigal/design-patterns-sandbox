2f523831a6a30ca6925113384990ba08410fef16
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-10 14:22:47 -0400

    notes

--- object-pool/main.py
+++ object-pool/main.py
@@ -1,3 +1,10 @@
 # search:
 # "union"
 # "free list"
+# "This looks a lot like the Flyweight pattern." --
+# "Flyweight objects are reused by sharing the same instance between multiple owners simultaneously.
+# The Flyweight pattern avoids duplicate memory usage by using the same object in multiple contexts."
+
+# The objects in a pool get reused too, but only over time. “Reuse” in the context of an object pool
+# means reclaiming the memory for an object after the original owner is done with it. With an object pool,
+# there isn’t any expectation that an object will be shared within its lifetime."
