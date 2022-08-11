2358985d1c7d2e4053c05a1cd905df4868e1d00b
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-11 14:35:57 -0400

    notes

--- spatial-partition/main.py
+++ spatial-partition/main.py
@@ -5,3 +5,8 @@
 # simple 1d rendidion - units on a line
 # array sort by unit location -> binary search (takes search from O(n^2) to O(n log n)
 # pigeonhole? -> O(n)?
+
+# "For a set of objects, each has a position in space. Store them in a spatial data structure
+# that organizes the objects by their positions. This data structure lets you efficiently query
+# for objects at or near a location. When an object’s position changes, update the spatial data structure
+# so that it can continue to find the object."
