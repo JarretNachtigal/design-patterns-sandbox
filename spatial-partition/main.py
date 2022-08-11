98612e8c824412730222b403a8b1d2aecf18d3ae
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-11 14:34:32 -0400

    notes

--- spatial-partition/main.py
+++ spatial-partition/main.py
@@ -1,3 +1,7 @@
 # "Efficiently locate objects by storing them in a data structure organized by their positions."
 
 # ex RTS games
+
+# simple 2d rendidion
+# array sort by unit location -> binary search (takes search from O(n^2) to O(n log n)
+# pigeonhole? -> O(n)?
