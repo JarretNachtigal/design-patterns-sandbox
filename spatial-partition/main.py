21ed9c041bb661224c7cd6ef0edccb8b8783ffc2
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-11 14:38:28 -0400

    notes

--- spatial-partition/main.py
+++ spatial-partition/main.py
@@ -10,3 +10,7 @@
 # that organizes the objects by their positions. This data structure lets you efficiently query
 # for objects at or near a location. When an object’s position changes, update the spatial data structure
 # so that it can continue to find the object."
+
+# used in static situations (game world) as well
+# used when "you have a set of objects that each have some kind of position and that you are doing enough
+# queries to find objects by location that your performance is suffering."
